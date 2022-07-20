1e7ec7645be9bcf05f9f5df95e83e816f292e98c
Andrea <andrea.juarez@gmail.com>
2022-07-20 23:17:10 +0000

    Primer subida

--- src/app.py
+++ src/app.py
@@ -1,4 +1,12 @@
-from utils import db_connect
-engine = db_connect()
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#%matplotlib inline 
 
-# your code here
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+from sklearn import metrics
